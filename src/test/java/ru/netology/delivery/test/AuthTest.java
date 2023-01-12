9ef82608f7cb7990105b3dbe82723a87e633d0d1
SobolB <79850869+SobolB@users.noreply.github.com>
2023-01-12 21:21:44 +0300

    Update AuthTest.java

--- src/test/java/ru/netology/delivery/test/AuthTest.java
+++ src/test/java/ru/netology/delivery/test/AuthTest.java
@@ -2,18 +2,12 @@ package ru.netology.delivery.test;
 
 
 import com.codeborne.selenide.Condition;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.netology.delivery.data.DataGenerator;
+
 
 import static com.codeborne.selenide.Selenide.$;
-import static io.restassured.RestAssured.given;
 import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.delivery.data.DataGenerator.Registration.getRegisteredUser;
 import static ru.netology.delivery.data.DataGenerator.Registration.getUser;
